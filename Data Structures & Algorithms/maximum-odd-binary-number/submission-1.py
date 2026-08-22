d82295fc9454ce60432d00c5196ed7dbc07385bf
Harsh Upreti <53615879+HungryBoy69@users.noreply.github.com>
2026-08-23 03:09:24 +0530

    Add: maximum-odd-binary-number - submission-1

---
+++ Data Structures & Algorithms/maximum-odd-binary-number/submission-1.py
@@ -0,0 +1,10 @@
+from collections import defaultdict
+class Solution:
+    def maximumOddBinaryNumber(self, s: str) -> str:
+        hashMap = defaultdict(int)
+        for ch in s:
+            hashMap[ch]+=1
+        num_one = hashMap['1'] - 1
+        
+        return '1'*num_one + '0'*hashMap['0'] + '1'*(hashMap['1'] - num_one)
+        
